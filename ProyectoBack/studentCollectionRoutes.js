950117501f7921e5170a6058d239624e31acab20
abraham28s <ab.soto@hotmail.com>
2018-04-17 21:16:00 -0500

    Server

---
+++ ProyectoBack/studentCollectionRoutes.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = function(app) {
+ var studentCollection = require('./studentCollectionController');
+
+ //app.route('/videojuegos')
+ //.get(studentCollection.obtener_estudiantes)
+ //.post(studentCollection.agregar_estudiante);
+
+//Primer Query
+ app.route('/videojuegos/:consola')
+ .get(studentCollection.obtenerPrimer);
+
+//Segundo Query
+ app.route('/videojuegos/busqueda/:palabraClave')
+ .get(studentCollection.buscar_palabra_clave);
+
+ app.route('/videojuegos/juegos/:consola')
+ .get(studentCollection.obtenerJuegos);
+
+ app.route('/videojuegos/juegosRegex/:palabraClave')
+ .get(studentCollection.buscar_palabra_clave_juegos);
+
+ app.route('/videojuegos/info/:juego')
+ .get(studentCollection.infoJuego);
+
+ app.route('/blogs')
+ .get(studentCollection.obtenerBlogs);
+
+ app.route('/consolas')
+ .get(studentCollection.getConsolas);
+
+ app.route('/juegos/:idjuego')
+ .get(studentCollection.infoJuego);
+
+ app.route('/juegosNombre/:juego')
+ .get(studentCollection.infoJuegoPorNombre);
+
+
+};
